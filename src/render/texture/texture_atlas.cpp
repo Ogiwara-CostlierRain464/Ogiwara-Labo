83a1fe7aff0c08fa0cd12ad19bbb95704a8e6510
ogiwara <yushiogiwara@gmail.com>
2020-06-09 22:39:24 +0900

    update texture_atlas.cpp

--- src/render/texture/texture_atlas.cpp
+++ src/render/texture/texture_atlas.cpp
@@ -1,1 +1,32 @@
 #include "texture_atlas.h"
+#include <array>
+
+labo::render::TextureAtlas::TextureAtlas(
+  const std::string &textureFileName) :
+  BasicTexture(textureFileName) {
+  sf::Image i;
+  if(i.loadFromFile("res/textures/" + textureFileName + ".png")){
+    throw std::runtime_error("Unable to open image: " + textureFileName);
+  }
+  loadFromImage(i);
+
+  imageSize = 256;
+  individualTextureSize = 16;
+}
+
+std::array<GLfloat, 8> labo::render::TextureAtlas::getTexture(
+  const sf::Vector2i &coords) {
+  static const GLfloat TEX_PRE_ROW =
+    (GLfloat) imageSize / (GLfloat) individualTextureSize;
+  static const GLfloat INDIVIDUAL_TEX_SIZE = 1.f / TEX_PRE_ROW;
+  static const GLfloat PIXEL_SIZE = 1.f / (float) imageSize;
+
+  GLfloat xMin = (coords.x * INDIVIDUAL_TEX_SIZE) + 0.5f * PIXEL_SIZE;
+  GLfloat yMin = (coords.y * INDIVIDUAL_TEX_SIZE) + 0.5f * PIXEL_SIZE;
+
+  GLfloat xMax = (xMin + INDIVIDUAL_TEX_SIZE) - PIXEL_SIZE;
+  GLfloat yMax = (yMin + INDIVIDUAL_TEX_SIZE) - PIXEL_SIZE;
+
+  return {xMax, yMax, xMin, yMax, xMin, yMin, xMax, yMin};
+}
+
--- src/render/texture/texture_atlas.h
+++ src/render/texture/texture_atlas.h
@@ -1,10 +1,20 @@
 #ifndef LABO_TEXTURE_ATLAS_H
 #define LABO_TEXTURE_ATLAS_H
 
+#include "basic_texture.h"
 
-class texture_atlas {
+namespace labo::render{
+class TextureAtlas: public BasicTexture{
+public:
+  explicit TextureAtlas(const std::string &textureFileName);
 
+  std::array<GLfloat, 8> getTexture(const sf::Vector2i &coords);
+
+private:
+  int imageSize;
+  int individualTextureSize;
 };
+}
 
 
 #endif //LABO_TEXTURE_ATLAS_H
