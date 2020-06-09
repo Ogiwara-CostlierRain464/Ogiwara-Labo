8acb0521e8d6f0a74a4f2ae4bae70c6c487fbbf3
ogiwara <yushiogiwara@gmail.com>
2020-06-09 15:53:14 +0900

    update experimental_space_generator.cpp

--- src/minecraft/level/generator/experimental_space_generator.cpp
+++ src/minecraft/level/generator/experimental_space_generator.cpp
@@ -5,7 +5,12 @@ void labo::minecraft::ExperimentalSpaceGenerator
 ::generateTerrainFor(Chunk &chunk) {
   for(int x = 0; x < CHUNK_SIZE; x++){
     for(int z = 0; z < CHUNK_SIZE; z++){
-      //chunk.setBlock(x, 0, z,)
+      chunk.setBlock(x, 0, z, Stone);
+      chunk.setBlock(x, 1, z, Stone);
     }
   }
+}
+
+int labo::minecraft::ExperimentalSpaceGenerator::getMinimumSpawnHeight() const noexcept {
+  return 2;
 }
