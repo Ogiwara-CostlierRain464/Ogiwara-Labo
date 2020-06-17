651ef5547dd3af9aa17cb3f61219ac759b579cf9
ogiwara <yushiogiwara@gmail.com>
2020-06-17 15:23:39 +0900

    add level_mesh_builder.cpp

---
+++ src/render/builder/level_mesh_builder.cpp
@@ -0,0 +1,1 @@
+#include "level_mesh_builder.h"
---
+++ src/render/builder/level_mesh_builder.h
@@ -0,0 +1,14 @@
+#ifndef LABO_LEVEL_MESH_BUILDER_H
+#define LABO_LEVEL_MESH_BUILDER_H
+
+namespace labo::render{
+
+class LevelMeshBuilder {
+public:
+  void pushToRenderer();
+};
+
+}
+
+
+#endif //LABO_LEVEL_MESH_BUILDER_H
