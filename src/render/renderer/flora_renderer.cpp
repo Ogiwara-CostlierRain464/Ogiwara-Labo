a74da17eee8b78f0e6e70ef5cdc29eb0a63d5a96
ogiwara <yushiogiwara@gmail.com>
2020-06-17 14:14:52 +0900

    add flora_renderer.cpp

---
+++ src/render/renderer/flora_renderer.cpp
@@ -0,0 +1,29 @@
+#include "flora_renderer.h"
+#include "../chunk_mesh.h"
+#include "../camera.h"
+
+void labo::render::FloraRenderer::add(
+  const ChunkMesh &mesh){
+  chunks.push_back(mesh.getContainer().getRenderInfo());
+}
+
+void labo::render::FloraRenderer::render(
+  const Camera &camera, float elapsedTime) {
+  if(chunks.empty()){
+    return;
+  }
+
+  glDisable(GL_BLEND);
+  glDisable(GL_CULL_FACE);
+  shader.useProgram();
+
+  shader.loadProjViewMatrix(camera.getProjectionViewMatrix());
+  shader.loadTime(elapsedTime);
+
+  for(auto &mesh: chunks){
+    glBindVertexArray(mesh.vao);
+    glDrawElements(GL_TRIANGLES, mesh.indicesCount, GL_UNSIGNED_INT, nullptr);
+  }
+
+  chunks.clear();
+}
---
+++ src/render/renderer/flora_renderer.h
@@ -0,0 +1,25 @@
+#ifndef LABO_FLORA_RENDERER_H
+#define LABO_FLORA_RENDERER_H
+
+#include <vector>
+#include "../mesh.h"
+#include "../shader/flora_shader.h"
+
+namespace labo::render{
+
+class ChunkMesh;
+class Camera;
+
+class FloraRenderer {
+public:
+  void add(const ChunkMesh &mesh);
+  void render(const Camera &camera, float elapsedTime);
+
+private:
+  std::vector<MeshContainer::RenderInfo> chunks;
+  FloraShader shader;
+};
+
+}
+
+#endif //LABO_FLORA_RENDERER_H
